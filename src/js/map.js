a23099b4363c7c6dea2187ccc1db1b93888fbdf7
Maksim Nadolynnyi <specimeniguess@gmail.com>
2018-08-01 20:33:42 +0300

    Done the Feedback section

---
+++ src/js/map.js
@@ -0,0 +1,5 @@
+function initMap() {
+    var uluru = {lat: 10.748182, lng: 16.5607998};
+    var map = new google.maps.Map(
+        document.getElementById('map'), {zoom: 1.51, center: uluru});
+  }
