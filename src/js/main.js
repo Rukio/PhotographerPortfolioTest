1179257ed7ebfb0648c50273a64dec366efa8d06
Maksim Nadolynnyi <specimeniguess@gmail.com>
2018-08-01 15:16:15 +0300

    Comment section done

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,17 @@
 $(document).ready(function(){
-    $(".owl-carousel").owlCarousel({
+    $(".slider-photo").owlCarousel({
         items: 1,
         stagePadding: 100,
         margin: 20,
         loop: true,
         center: true,
         autoWidth: true,
+        nav: true,
+        navText: ['<i class="fas fa-angle-left"></i>', '<i class="fas fa-angle-right"></i>']
+    });
+    $(".comments__slider").owlCarousel({
+        items: 2,
+        loop: true,
         nav: true,
         navText: ['<i class="fas fa-angle-left"></i>', '<i class="fas fa-angle-right"></i>']
     });
